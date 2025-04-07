fe3f9bbb94b334608224b97f92a288c54774161a
The-vishnu <143195086+The-vishnu@users.noreply.github.com>
2025-04-07 17:34:40 +0000

    new code

---
+++ Basics/Variable.js
@@ -0,0 +1,9 @@
+const accountId = 123445
+let accountMail = "work@gmail.com"
+var accountPassword = "23244" 
+accountCity = "Jaipur "
+
+
+console.table([accountId, accountMail, accountPassword, accountCity])
+
+// console.log(accountId)
---
+++ Basics/conversion.js
@@ -0,0 +1,6 @@
+let score = "34234jmhvjhv"
+
+let valeNumber = Number(score)
+console.log(typeof(score))
+console.log(typeof(valeNumber))
+console.log(valeNumber)
