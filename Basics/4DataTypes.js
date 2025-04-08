b2d6127d103847238fdce9e644fd57f285f6b119
The-vishnu <143195086+The-vishnu@users.noreply.github.com>
2025-04-08 04:48:42 +0000

    Renamed and new files

---
+++ Basics/4DataTypes.js
@@ -0,0 +1,30 @@
+// Premitive Data Types
+
+// 7 types: string, Number, boolean, Null, Undefined, Symbol, BigInt
+
+const score = 234
+const scorValue = 23.2
+const isLoggined = false 
+const outSideaTemp = null
+let userName;
+
+const bigNumber = 39849289289234798729n
+const id = Symbol('616')
+const anotherId = Symbol('616')
+
+console.log(id === anotherId)
+
+
+// Arryas, Objects, Function 
+
+const heros = ["Iron man", "Loki", "Hulk", "Dr. strange"];
+
+let obj = {
+    name: "Tony Stark ",
+    age: 49,
+
+}
+
+const myFunction = function(){
+    console.log("server start...")
+}
