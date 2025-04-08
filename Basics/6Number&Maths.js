21393430f59906e7bf8dd67163f1331f3790768b
The-vishnu <143195086+The-vishnu@users.noreply.github.com>
2025-04-08 07:04:40 +0000

    practice of mahs and numbers

--- Basics/6Number&Maths.js
+++ Basics/6Number&Maths.js
@@ -1,4 +1,22 @@
+// Numbers 
+
 const score = 626
 const balance = new Number(100)
+const otherNumber = 123.33765
+const hunders = 100000
 
 console.log(balance)
+console.log(score)
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+console.log(otherNumber.toPrecision(3))
+console.log(hunders.toLocaleString('en-IN'))
+
+// Maths 
+
+console.log(Math)
+console.log(Math.abs(4))
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.random() * 10)
