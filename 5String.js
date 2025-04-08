71de533b2e0f6c25681810888c219f584456a4fb
The-vishnu <143195086+The-vishnu@users.noreply.github.com>
2025-04-08 05:31:45 +0000

    new consept

--- 5String.js
+++
@@ -1,4 +0,0 @@
-const name = "Tony"
-const repoCount = 50
-
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
---
+++ Basics/5String.js
@@ -0,0 +1,10 @@
+const name = "Tony "
+const repoCount = 50
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const name2 = new String('iron man')
+
+console.log(name2[0])
+console.log(name2.length)
+console.log(name2.toUpperCase())
