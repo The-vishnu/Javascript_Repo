0c0746ae44b983604ddd36361d2ef3d65a908939
The-vishnu <143195086+The-vishnu@users.noreply.github.com>
2025-04-18 17:56:42 +0000

    done with the project

--- FetchAPI/script.js
+++ FetchAPI/script.js
@@ -1,17 +1,57 @@
-
-const url = "https://api.github.com/users/The-vishnu";
+const Name = document.getElementById('name')
+const imgSrc = document.getElementById('avatar');
+const bio = document.getElementById('bio');
+const followers = document.getElementById('followers');
+const repos = document.getElementById('repos');
 
 // let promice = fetch(url)
 // console.log(promice)
 
-const getinfo = async function () {
-    console.log("fetching data...");
-    let responce = await fetch(url);
-    console.log(responce);
 
-    let data = await responce.json();
 
-    console.log(data)
+const updateUI = function(data) {
+
+    imgSrc.src = data.avatar_url;
+    Name.textContent = data.name;
+    followers.textContent = `Followers: ${data.followers}`;
+    repos.textContent = `repos: ${data.public_repos}`;
+
+    if(data.bio == null){
+        bio.textContent = "No Bio Available!";
+    }else{
+        bio.textContent = data.bio;
+    }
+    document.getElementById('profile-card').style.display = 'block';
+    
 }
 
-getinfo();
+
+const getinfo = async function () {
+    const userName = document.getElementById('username').value.trim();
+
+    if (!userName) {
+        alert("Please enter a GitHub username!");
+        return;
+    }
+    const url = `https://api.github.com/users/${userName}`;
+
+    try {
+        const responce = await fetch(url);
+        if(!responce.ok){
+            throw new Error("User not found!");
+        }
+        const data = await responce.json();
+        updateUI(data);
+    } catch (error) {
+        alert("Failed to fetch data!");
+        profileCard.style.display = 'none';
+        
+    }
+    // const data = await responce.json();
+    // console.log(data);
+
+    // updateUI(data);
+}
+
+document.querySelector('button').addEventListener('click', getinfo);
+
